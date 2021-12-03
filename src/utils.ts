63791a99aaa86e1255bd9dc3bc00e2c56c26ebba
Juan Ojeda <juan@juanojeda.com>
2021-12-03 11:41:24 +1100

    make logger an infinite args fn

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,5 @@ export type ErrorOr<T> = Error | T;
 export const isError = (maybeError: unknown): maybeError is Error =>
   maybeError instanceof Error;
 
-export const logger = (msg: unknown): void => console.log(msg);
+// eslint-disable-next-line functional/functional-parameters
+export const logger = (...msg: readonly unknown[]): void => console.log(...msg);
